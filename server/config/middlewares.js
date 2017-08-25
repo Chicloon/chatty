f655f5366ee046587a6a82221322804d665c2177
Chicloon <Chicloon@gmail.com>
2017-08-25 13:14:42 +0900

    .

--- server/config/middlewares.js
+++ server/config/middlewares.js
@@ -29,32 +29,6 @@ async function auth(req, res, next) {
   }
 }
 
-const addUser = async (req, res, next) => {
-  const token = req.headers['x-token'];
-  console.log(token);
-  if (token) {
-    try {
-      const { user } = jwt.verify(token, constants.SECRET);
-      req.user = user;
-    } catch (err) {
-      const refreshToken = req.headers['x-refresh-token'];
-      const newTokens = await refreshTokens(
-        token,
-        refreshToken,
-        constants.SECRET
-      );
-      if (newTokens.token && newTokens.refreshToken) {
-        res.set('Access-Control-Expose-Headers', 'x-token, x-refresh-token');
-        res.set('x-token', newTokens.token);
-        res.set('x-refresh-token', newTokens.refreshToken);
-      }
-      req.user = newTokens.user;
-    }
-  }
-  next();
-};
-
-
 export default app => {
   app.use(bodyParser.json());
   app.use(auth);
